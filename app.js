f388d1e2719c2140c24bbe9d5288c20954b3ae08
reddberckley <danielonodje@gmail.com>
2016-08-26 20:12:09 +0100

    add document route

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var documents = require('./routes/documents');
 
 var app = express();
 
